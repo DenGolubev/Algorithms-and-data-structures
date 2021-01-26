540339f9ec9dde94d2cdcc1dea584be924bd8a41
Денис Голубев (Работа) <amigo36@yandex.ru>
2021-01-26 16:27:26 +0300

    Написал класс Tree<T>

---
+++ Lesson4_Exercise 2/Tree.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson4_Exercise_2
+{
+    class Tree<T>
+    {
+        public T Value;
+        public Tree<T> NodeLeft; 
+        public Tree<T> NodeRight; 
+        public Tree<T> NodeParent;
+
+    }
+}
