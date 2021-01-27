b5a25e0689f24625c489986be9d90068fd9631ed
Денис Голубев <amigo36@yandex.ru>
2021-01-28 01:07:59 +0300

    Lesson4_Exercise_2 в процессе

--- Lesson4_Exercise 2/Tree.cs
+++ Lesson4_Exercise 2/Tree.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,12 +7,25 @@ using System.Threading.Tasks;
 
 namespace Lesson4_Exercise_2
 {
-    class Tree<T>
+    class TreeNode<T>: IComparable<T> where T: IComparable<T>
     {
-        public T Value;
-        public Tree<T> NodeLeft; 
-        public Tree<T> NodeRight; 
-        public Tree<T> NodeParent;
+        public T Value { get; set; }
+        public TreeNode<T> NodeLeft { get; set; }
+        public TreeNode<T> NodeRight { get; set; }
 
+        public int CompareTo(T other)
+        {
+            return CompareTo(other);
+        }
+
+        public TreeNode<int> Tree(int node)
+        {
+            TreeNode<int> newNode = null;
+            if (node == 0) return null;
+            if (Value == null) return Value = node;
+           
+            return newNode;
+        }
     }
+
 }
