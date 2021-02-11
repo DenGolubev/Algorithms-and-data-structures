5ce75b11350c91ecd2f9512f706654f90ebf9976
Денис Голубев (Работа) <amigo36@yandex.ru>
2021-02-11 17:35:52 +0300

    Урок 7

---
+++ Lesson7_Exercise 1/Field.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson7_Exercise_1
+{
+    class Field
+    {
+        public void myField()
+        {
+            Console.WriteLine("введите колличество клеток");
+            int a = int.Parse(Console.ReadLine());
+            int[][] mass = new int[a][];
+            //var point = 
+            for (int i = 0; i < a; i++)
+            {
+                for (int j = 0; j < a; j++)
+                {
+                    mass[i][j] = 0;
+                }
+            }
+
+            //mass[0][0] = 1;
+            //if (mass[0][0] == 1)
+            //{
+            //    for (int i = 0; i < mass.GetLength(0); i++)
+            //    {
+
+            //        for (int y = 0; y < mass.GetLength(1); y++)
+            //        {
+
+            //            if (mass[i][y] == 1)
+            //            {
+                            
+            //            }
+            //        }
+            //    }
+            //}
+            //else
+            //{
+            //    mass[0][0] = 1;
+            //}
+            
+            for (int i = 0; i < a; i++)
+            {
+                for (int j = 0; j < a; j++)
+                {
+                    Console.Write(mass[i][j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ Lesson7_Exercise 1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson7_Exercise_1
+{
+    class Program
+    {
+        static Field field = new Field();
+        static void Main(string[] args)
+        {
+            field.myField();
+        }
+    }
+}
